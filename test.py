6fb664a825af643b71f76bc6f50f9ac659c204ba
saumya bahuguna <bahuguns@tcd.ie>
2021-12-26 19:59:25 +0000

    some updates

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("This line will be printed.=" "+ 2-2")
+from github import Github
+
+
+g= Github("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")
+usr = g.get_user()
+if usr.login is not None:
+ print("user:" + usr.login)
+if usr.name is not None:
+ print("fullname: " +usr.name)
+if usr.location is not None:
+  print("location: " +usr.location)
+if usr.company is not None:
+  print("company: " +usr.company)
