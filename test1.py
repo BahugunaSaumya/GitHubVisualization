46ea9dadcacb4d348ae85edd3b512fd8b3629c65
saumya bahuguna <bahuguns@tcd.ie>
2022-01-13 00:56:11 +0000

    Some initial edits and important details in readme.md

--- test1.py
+++ test1.py
@@ -39,15 +39,7 @@ print(g.get_rate_limit())
 
 db = client.classDB
 #def _sum(arr): 
-      
-    # initialize a variable
-    # to store the sum
-    # while iterating through
-    # the array later
- #   sum=0
-      
-    # iterate through the array
-    # and add each element to the sum variable
+
     # one at a time
   #  for i in arr:
 #         sum = sum + i
